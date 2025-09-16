3c91ffa377b5068d871d139860c49e934d3a80c3
fourWayz <157867069+fourWayz@users.noreply.github.com>
2025-09-16 20:21:42 +0000

    fix eslint error

--- client/app/components/create/ConfirmStep.tsx
+++ client/app/components/create/ConfirmStep.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+
 "use client";
 import { useState } from "react";
 import { useCreateFlow } from "@/app/context/CreateFlowContext";
--- client/app/content/[id]/page.tsx
+++ client/app/content/[id]/page.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+
 "use client";
 
 import { use, useEffect, useState } from "react";
