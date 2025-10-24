5ae27f6787e562d5a0efc3bcbd7a26d487d053ce
fourWayz <157867069+fourWayz@users.noreply.github.com>
2025-10-24 12:21:11 +0000

    improved payment flow

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -4,17 +4,36 @@ import { WagmiProvider } from 'wagmi'
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import '@rainbow-me/rainbowkit/styles.css';
 import { createConfig, http } from 'wagmi'
-import { zeroGGalileoTestnet, zeroGMainnet } from 'viem/chains'
 import { injected, walletConnect } from 'wagmi/connectors'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import 'sweetalert2/dist/sweetalert2.min.css';
 
+const ogChainConfig = {
+  id: 16602,
+  name: "OG Galileo Testnet",
+  network: "ogchain",
+  nativeCurrency: {
+    symbol: "OG",
+    name: "OG Token",
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://evmrpc-testnet.0g.ai"],
+    },
+    public: {
+      http: ["https://evmrpc-testnet.0g.ai"],
+    },
+  },
+} as const;
+
+
+
 const queryClient = new QueryClient()
 const wagmiConfig = createConfig({
-    chains: [zeroGGalileoTestnet, zeroGMainnet] as const,
+    chains: [ogChainConfig] as const,
     transports: {
-        [zeroGGalileoTestnet.id]: http(),
-        [zeroGMainnet.id]: http(),
+        [ogChainConfig.id]: http(),
     },
     connectors: [
         injected(),
