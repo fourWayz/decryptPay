431bbb4b7df3a2f16f3734f2e2f7f4a51aa27df0
fourWayz <157867069+fourWayz@users.noreply.github.com>
2025-10-24 11:50:28 +0000

    resolve type error

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,21 +1,20 @@
 "use client"
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { WagmiProvider } from 'wagmi'
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import '@rainbow-me/rainbowkit/styles.css';
 import { createConfig, http } from 'wagmi'
-import { filecoin, filecoinCalibration } from 'viem/chains'
+import { zeroGGalileoTestnet, zeroGMainnet } from 'viem/chains'
 import { injected, walletConnect } from 'wagmi/connectors'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import 'sweetalert2/dist/sweetalert2.min.css';
 
 const queryClient = new QueryClient()
 const wagmiConfig = createConfig({
-    chains: [filecoin, filecoinCalibration] as const,
+    chains: [zeroGGalileoTestnet, zeroGMainnet] as const,
     transports: {
-        [filecoin.id]: http(),
-        [filecoinCalibration.id]: http(),
+        [zeroGGalileoTestnet.id]: http(),
+        [zeroGMainnet.id]: http(),
     },
     connectors: [
         injected(),
